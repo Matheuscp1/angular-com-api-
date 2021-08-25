489b7859b9e6b1862b23a2dacdf46f68650908ec
Matheus <matheusproprentner@gmail.com>
2021-08-24 22:08:45 -0300

    route get user, route post user, route auth

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,19 @@
 const express = require('express')
 const app = express()
 const sequelize = require('./database/connection')
-const UserModel = require('./model/User')
+
 const cors = require('cors')
+const route = require('./router/router')
 sequelize.authenticate().then(() => {
     console.log('Conexão estabelicida.')
 }).catch((error) => {
 
     console.error('Erro na conexão:', error);
 })
-
-
+app.use(cors())
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+app.use(route)
 app.listen(8080, (erro) => {
     if (erro) {
         console.log(erro)
--- backend/router/router.js
+++ backend/router/router.js
@@ -1,18 +1,83 @@
 const express = require('express')
 const route = express.Router()
-const UserModel = require('../models/User')
+const UserModel = require('../model/User')
 const bcrypt = require('bcryptjs')
 
 route.get("/", function (req, res) {
     res.send('<h1> Bem vindo ao api :3 </h1>')
 })
 
+
+route.get('/user/:email?', (req, res) => {
+    email = req.params.email
+    if (email) {
+        UserModel.findOne({
+            where: {
+                email
+            },
+            raw: true
+        }).then(user => {
+           
+            res.status(200).json(!!user)
+        })
+    } else {
+        UserModel.findAll({
+            raw: true, order: [
+                ['id', 'desc']
+            ]
+        }
+        ).then(users => {
+            users.forEach(user => {
+                delete user.password
+            })
+            res.status(200).json(users)
+        })
+    }
+
+})
+
+
+route.post('/auth', (req, res) => {
+    let { password, email } = req.body
+    UserModel.findOne({
+        where: {
+            email
+        },   
+        raw: true 
+    }, ).then(user => {
+        if (!user) {
+            res.status(404).json({ error: 'usuario não encontrado' })
+        } else {
+            let compare = bcrypt.compareSync(password, user.password)
+            if (compare) {
+                delete user.password
+                jwt.sign({ id: user.id, email: user.email }, 'testeJWT', { expiresIn: '2h' }, (err, token) => {
+                    if (err) {
+                        res.status(401).json(err)
+                    } else {
+                      
+                        res.set('Access-Control-Expose-Headers', '*')
+                        res.set('x-access-token', token)
+                        return res.status(200).json({user,token}); 
+                        
+                    }
+                })
+            } else {
+                res.send('usuario não encontrado').status(404)
+            }
+        }
+    }).catch(e => {
+        res.status(400).json({ error: e.errors.map(err => err.message) })
+    })
+})
+
+
 route.post('/user', (req, res) => {
-    const { email, nick_name, name, password } = req.body
+    const { email, name, password } = req.body
     let salt = bcrypt.genSaltSync(10)
     let hash = bcrypt.hashSync(password, salt)
     UserModel.create({
-        email, nick_name, name, password: hash
+        email, name, password: hash
     }).then(() => {
         console.log('criado com sucesso')
         res.status(201).json(req.body)
